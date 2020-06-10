5f472edca40e8edfd03d6651987daa6bf4716c70
Jerome Bravo <jeromebravo0608@gmail.com>
2020-06-10 10:26:24 +0800

    change database

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const indexRoutes = require("./routes/index"),
       orderRoutes = require("./routes/orders")
       salesRoutes = require("./routes/sales");
 
-mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost/battery_shop", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useFindAndModify", false);
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
